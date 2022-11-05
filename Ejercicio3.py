2957d8869fc30e2f2b995264aa59bae56317c4f6
evamgarcia <emgarcia2911@gmail.com>
2022-11-05 18:23:55 +0100

    Update Ejercicio3.py

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -14,5 +14,33 @@ Nave_Tie = nave("Caza_Tie", 6, 1, 1)
 Nave_Ala_x = nave("Ala_x", 13, 1, 1)
 Nave_Destructor = nave("Destructor_estelar", 1300, 5400, 5400)
 
-lista= [Nave_Destructor, Nave_Ala_x, Nave_Tie, Nave_Estrella, Nave_Halcon]
+lista_naves = [Nave_Destructor, Nave_Ala_x, Nave_Tie, Nave_Estrella, Nave_Halcon]
 print(Nave_Estrella, Nave_Destructor)
+
+lista_ordenada_pasajeros = sorted(lista_naves, key=lambda x: x.cantidad_pasajeros, reverse=True)
+lista_ordenada_tripulacion = sorted(lista_naves, key=lambda x: x.tripulacion, reverse=True)
+lista_ordenada_largo = sorted(lista_naves, key=lambda x: x.largo)
+
+def mostrar_cinco_naves_mas_pasajeros (e):
+    print("Estas son las cinco naves con mas pasajeros: ")
+    for i in range(5):
+        if i < len(e):
+            print(e[i])
+
+def mostrar_naves_mas_tripulacion(e):
+    print("Esta es la nave con mas tripulacion: ", e[0])
+
+def mostrar_naves_mayor_menor_tamaño(e):
+    print("Esta es la nave con mas tamaño: ", e[len(e)-1], "nave con menos tamaño: ", e[0])
+
+mostrar_cinco_naves_mas_pasajeros(lista_ordenada_pasajeros)
+mostrar_naves_mas_tripulacion(lista_ordenada_tripulacion)
+mostrar_naves_mayor_menor_tamaño(lista_ordenada_largo)
+
+def mostrar_mas_seis_pasajeros(e):
+    for i in e:
+        if i.cantidad_pasajeros >= 6:
+            print(i)
+mostrar_cinco_naves_mas_pasajeros(lista_naves)
+mostrar_naves_mas_tripulacion(lista_naves)
+mostrar_naves_mayor_menor_tamaño(lista_naves)
