b449502eae8086fb4bfa92f34640e6eb76793769
evamgarcia <emgarcia2911@gmail.com>
2022-11-06 13:29:27 +0100

    Update Ejercicio2.py

---
+++ Ejercicio2.py
@@ -0,0 +1,3 @@
+def calcular_determinante (matriz):
+    return (matriz[0][0]*matriz[1][1]*matriz[2][2])+ (matriz[1][0]*matriz[2][1]*matriz[0][2])+(matriz[0][1]*matriz[1][2]*matriz[2][0]) - (matriz[0][2]*matriz[1][1]*matriz[2][0]) - (matriz[0][1]*matriz[1][0]*matriz[2][2]) - (matriz[0][0]*matriz[2][1]*matriz[1][2])
+print("El determinante de la matriz dada es: " + str(calcular_determinante[[3, 4, 3], [1, 2, 3], [0, 2,3]]))
